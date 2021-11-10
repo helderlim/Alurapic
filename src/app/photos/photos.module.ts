e3317952341b7615433f38eab17bfc08e72a2cb4
helderlim <helderfelima16@gmail.com>
2021-11-10 20:15:23 -0300

    feat: BrowserModule & CommonModule

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from "@angular/core";
+import { CommonModule } from '@angular/common';
 
 import { PhotoComponent } from "./photo/photo.component";
 import { PhotoListComponent } from './photo-list/photo-list.component';
@@ -10,6 +11,11 @@ import { PhotoListComponent } from './photo-list/photo-list.component';
         PhotoComponent, 
         PhotoListComponent 
     ],
-    imports: [HttpClientModule] 
+    imports: [
+        HttpClientModule,
+        //boa pratica sempre importar no modulo o CommonModulo para tornar disponivel as diretivas do Angular
+        //nunca importe BrowserModule a nao ser no app.module o principal 
+        CommonModule
+    ] 
 })
 export class PhotosModule {}
